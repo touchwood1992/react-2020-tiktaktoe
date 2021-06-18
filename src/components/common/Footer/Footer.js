919cd15161d6910068e9d22218389233586d6f41
touchwood1992 <39399343+touchwood1992@users.noreply.github.com>
2021-06-18 12:21:22 +0530

    Footer copyright changes
    Footer copyright changes done

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-warning pt-3 pb-3 text-center site-footer">
-      <div className="container">&copy;2020 prashant parmar</div>
+      <div className="container">&copy;2020 Test Copy</div>
     </footer>
   );
 };
